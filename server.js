cb8fe40fb77aec6dfa693528a17540b9f106e1b1
AlineL1ma <166144786+AlineL1ma@users.noreply.github.com>
2025-04-04 11:42:09 -0300

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('MongoDB conectado!'))
-  .catch(err => console.log('Erro ao conectar:', err));
